589f0d36b9bb15665d84f0fc7bb401928d704449
Narendra Joshi <narendraj9@gmail.com>
2016-10-26 19:40:42 +0530

    Fix hledger-mode description.

--- hledger-mode.el
+++ hledger-mode.el
@@ -9,9 +9,15 @@
 
 ;;; Commentary:
 ;;
-;; This is a major mode writing hledger journal files.  You must have
-;; hledger installed to be able to create the reports: balancesheet,
-;; income statement, etc.
+;; A major mode for writing hledger journal files. It generates some
+;; useful reports along with some financial ratios that can help you
+;; keep a check on your financial health. This is an attempt to
+;; organize personal finances for Emacs users. If you don't like this,
+;; try `ledger-mode'.
+;;
+;; Note: You must have hledger installed to be able to create the
+;;       reports: overall report, daily report, balancesheet, income
+;;       statement, etc.
 ;;
 
 ;;; Code:
